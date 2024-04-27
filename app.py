ab0fcf1e9a914208486ddf13a2e46569d3f8b779
WooSeongYun <83205343+Woosyun@users.noreply.github.com>
2024-04-27 09:43:06 +0900

    represent output in two columns

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import json
 
 def show():
   col1, col2 = st.columns(2)
-  st.image("image0001.png", width=100)
+  with col1:
+    st.image("image0001.png", width=100)
   with open("image0001.json", "r", encoding="utf-8") as f:
     file = json.load(f)
-  st.write("Type:", file["type"])
-  # st.write("Ingredients:", file["ingredients"])
-  st.write("Alcohol Level:", file["alcohol_level"])
-  st.write("Volume:", file["volume"])
-  st.write("Introduction:", file["introduction"])
-  st.write("Brewery:", file["brewery"])
-  st.write("Homepage:", file["homepage"])
+  with col2:
+    st.write("Type:", file["type"])
+    # st.write("Ingredients:", file["ingredients"])
+    st.write("Alcohol Level:", file["alcohol_level"])
+    st.write("Volume:", file["volume"])
+    st.write("Introduction:", file["introduction"])
+    st.write("Brewery:", file["brewery"])
+    st.write("Homepage:", file["homepage"])
 
 alcohol_level = st.slider('alcohol level : ', 0, 100, 1)
 sweetness = st.slider('select sweetness you want', 0, 100, 1)
